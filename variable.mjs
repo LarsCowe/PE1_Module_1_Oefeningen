2ee0f49f7bb0c029b762d8d1590d2f23b4ff5920
Lars <lars.cowe@hotmail.com>
2025-11-20 15:12:17 +0100

    Fix duplicate declaration of isGeregistreerd boolean variable

--- variable.mjs
+++ variable.mjs
@@ -28,6 +28,7 @@ let isActief = true;
 let isGeregistreerd = false;
 let isPremiumLid = true;
 let isBeheerder = false;
+let isGeregistreerd = false;
 
 console.log("\n=== BOOLEAN VARIABELEN ===");
 console.log("Is student:", isStudent);
